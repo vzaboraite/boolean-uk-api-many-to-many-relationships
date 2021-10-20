fb5a66369d5ceb5a7218da5c04ff71c2eefb4d4f
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-10-20 20:55:55 +0100

    Setup routers for patients, doctors and appointments endpoints.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,38 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const express = require("express")
-const cors = require("cors")
-const morgan = require("morgan")
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
-const app = express()
+const patientsRouter = require("./routes/patient/router");
+const doctorsRouter = require("./routes/doctor/router");
+const appointmentsRouter = require("./routes/appointment/router");
+
+const app = express();
 
 /* SETUP MIDDLEWARE */
 
-app.disable("x-powered-by")
+app.disable("x-powered-by");
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(morgan("dev"))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 /* SETUP ROUTES */
 
+app.use("/patients", patientsRouter);
+app.use("/doctors", doctorsRouter);
+app.use("/appointments", appointmentsRouter);
+
 app.get("*", (req, res) => {
-  res.json({ ok: true })
+  res.json({ ok: true });
 });
 
 /* START SERVER */
 
-const port = process.env.PORT || 3030
+const port = process.env.PORT || 3030;
 
 app.listen(port, () => {
-  console.log(`\n🚀 Server is running on http://localhost:${port}/\n`)
+  console.log(`\n🚀 Server is running on http://localhost:${port}/\n`);
 });
---
+++ src/routes/patient/router.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  console.log("Working!!!");
+
+  res.json({ patient: true });
+});
+
+module.exports = router;
